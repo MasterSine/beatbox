1a1563633a543c4ac759e0d14a16f7e44725321f
MasterSine <86109071+MasterSine@users.noreply.github.com>
2023-12-03 04:17:14 -0800

    Create song_BPM.py

---
+++ song_BPM.py
@@ -0,0 +1,34 @@
+import librosa
+import os
+
+path = r"INSERT PATH HERE"
+length = len(path)
+
+os.chdir(path)
+songlist = []
+
+def roundtofive(number):
+    return 5 * round(number / 5)
+
+def temporder(l):
+    l.sort(key=lambda a: a[1])
+    return l
+
+for file in os.listdir():
+    if file.endswith(".mp3"):
+        file_path = os.path.join(path, file)
+
+        # Get BPM for each song
+        y, sr = librosa.load(file_path)
+        tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+
+        songName = file_path[length + 1: len(file_path) - 4]
+
+        tc = (f"{songName}", roundtofive(tempo))
+        songlist.append(tc)
+
+songlist = temporder(songlist)
+
+# Print the sorted list
+for song in songlist:
+    print(f"{song[0]} - BPM: {song[1]}")
